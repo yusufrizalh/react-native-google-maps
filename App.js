bd78aeb113cc33743c6ff69569e6d8df7cadca74
Yusuf Rizal <yusufrizalh@gmail.com>
2019-11-01 15:30:47 +0700

    react native google maps add img location

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   View,
   Text,
   StatusBar,
+  Image
 } from 'react-native';
 
 import {
@@ -19,26 +20,100 @@ import {
 import MapView from 'react-native-maps';
 import {Marker} from 'react-native-maps';
 
+const markerImg = require('./imgs/compass.png');
+
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      markers: [
+        {
+          title: 'UPN Veteran',
+          description: 'Universitas Pembangunan Nasional Veteran Surabaya',
+          coordinates: {
+            latitude: -7.333088,
+            longitude: 112.789529,
+          },
+        },
+        {
+          title: 'Perumahan Puri Mas',
+          description: 'Perumahan di Daerah Rungkut Surabaya',
+          coordinates: {
+            latitude: -7.336288,
+            longitude: 112.785726,
+          },
+        },
+        {
+          title: 'Richeese Factory Rungkut Madya',
+          description: 'Rumah Makan Cepat Saji di Rungkut',
+          coordinates: {
+            latitude: -7.3018866951694,
+            longitude: 112.77376851432145,
+          },
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <View style={styles.MainContainer}>
-        <MapView style={styles.mapStyle}
-          showUserLocation={false}
+        <MapView
+          style={styles.mapStyle}
+          showsUserLocation={true} 
+          showsMyLocationButton={true}
           zoomEnable={true}
           zoomControlEnabled={true}
+          minZoomLevel={5} 
+          maxZoomLevel={15} 
           initialRegion={{
-            latitude: -7.333088,
-            longitude: 112.789529,
+            latitude: -7.3333751,
+            longitude: 112.7894965,
             latitudeDelta: 0.09,
             longitudeDelta: 0.04,
           }}>
-            <Marker
+          {/* <Marker onPress={(event) => console.log(event.nativeEvent.coordinate)} 
               coordinate={{ latitude: -7.333088, longitude: 112.789529 }}
+              pinColor={'#abc555'}
               title={"UPN Veteran"}
               description={"Universitas Pembangunan Nasional Veteran Surabaya"}
              />
-          </MapView>
+
+             <Marker onPress={(event) => console.log(event.nativeEvent.coordinate)} 
+              coordinate={{ latitude: -7.336288, longitude: 112.785726 }}
+              pinColor={'#cba567'}
+              title={"Perumahan Puri Mas"}
+              description={"Perumahan di Daerah Rungkut Surabaya"}
+             />
+
+             <Marker onPress={(event) => console.log(event.nativeEvent.coordinate)} 
+              coordinate={{ latitude: -7.3018866951694, longitude: 112.77376851432145 }}
+              pinColor={'#6432ed'}
+              title={"Richeese Factory Rungkut Madya"}
+              description={"Richeese Factory Rungkut Madya"}
+             />
+
+             <Marker onPress={(event) => console.log(event.nativeEvent.coordinate)} 
+              coordinate={{ latitude: -7.35055039401, longitude: 112.78773745867933 }}
+              title={"Kebun Bibit Wonorejo"}
+              description={"Kebun Bibit Wonorejo"}
+             /> */}
+
+            {
+              this.state.markers.map(marker => (
+                <Marker
+                  coordinate={marker.coordinates} 
+                  title={marker.title} 
+                  description={marker.description} 
+                  pinColor={'#6432ed'} 
+                  image={markerImg} 
+                  centerOffset={{ x: -18, y: -60 }}
+                  anchor={{ x: 0.69, y: 1 }} 
+                  onpress={() => this.setState({ marker1: !this.state.marker1 })}
+                 />
+              ))
+            }
+        </MapView>
       </View>
     );
   }
@@ -60,5 +135,7 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     bottom: 0,
+    width: '100%',
+    height: '100%',
   },
 });
